2662b97bda435e9519554b413e617ef33289b345
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2025-12-03 15:12:55 -0700

    improve design; add notes

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,49 @@
 use std::collections::HashMap;
 
+const DEFAULT_SIZE: u8 = 3;
+
 struct Board {
     xsize: u32, // horizontal size (grows to right)
     ysize: u32, // vertical size (grows down)
-    board_map: HashMap<(u32, u32), Tile>,
+    board_map: HashMap<Coordinate, PlayerTile>, // invariant: `board_map` stores precisely `xsize` * `ysize` entries
+                                                // board_map.get(&Coordinate{ x, y }) should never return None
+                                                // so if it does
+                                                // .unwrap() -> crash 
+                                                // .expect("err msg") -> print err msg
 }
 
 #[derive(Debug, Clone, PartialEq)]
-enum Tile {
+enum PlayerTile {
     Hidden,
     Flagged,
-    Revealed (i32), // -1 if mine, values >= 0 indicated number of mines in vincinity
+    Revealed(RevealedTile) // improved design from `Revealed(i8)`
 }
 
-impl Default for Board {
-    fn default() -> Self {
-        let mut board_map = HashMap::new();
-
-        for x in 0..3 {
-            for y in 0..3 {
-                board_map.insert((x, y), Tile::Hidden);
-            }
-        }
-
-        Board {
-            xsize: 3,
-            ysize: 3,
-            board_map,
-        }
-    }
+#[derive(Debug, Clone, PartialEq)]
+enum RevealedTile {
+    Hint(i8), // i8 suffices since # of mines in neighboring tiles cannot exceed 8
+    Mine
 }
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+struct Coordinate { x: u32, y: u32 }
 
 impl Board {
+    fn default() -> Self {
+        Board::new(DEFAULT_SIZE as u32, DEFAULT_SIZE as u32) // u8 -> u32 upcase (safe)
+    }
+
     fn new(xsize: u32, ysize: u32) -> Self {
-        let mut board_map = HashMap::new();
+        let mut board_map = HashMap::new(); // mutate locally
+                                                                             // while keeping the functional style globally
+                                                                             // by generating a new `board_map` whenever a player makes a move 
 
         for x in 0..xsize {
             for y in 0..ysize {
-                board_map.insert((x, y), Tile::Hidden);
+                board_map.insert(Coordinate{ x, y }, PlayerTile::Hidden);
             }
         }
-        
+
         Board {
             xsize,
             ysize,
@@ -51,11 +54,13 @@ impl Board {
     fn print(&self) {
         for y in 0..self.ysize {
             for x in 0..self.xsize {
-                match self.board_map.get(&(x, y)) {
-                    Some(Tile::Hidden) => print!("? "),
-                    Some(Tile::Flagged) => print!("! "),
-                    Some(Tile::Revealed(value)) => print!("{} ", value),
-                    None => print!(". "),  // Empty cell
+                match self.board_map.get(&Coordinate{ x, y }).unwrap() {
+                    PlayerTile::Hidden => print!("? "),
+                    PlayerTile::Flagged => print!("! "),
+                    PlayerTile::Revealed(value) => match value {
+                        RevealedTile::Hint(n) => print!("{}", n),
+                        RevealedTile::Mine => print!("*")
+                    }
                 }
             }
             println!();  // New line after each row
@@ -64,6 +69,9 @@ impl Board {
 }
 
 fn main() {
-    let b = Board::default();
-    b.print();
+    let db = Board::default();
+    db.print();
+    println!();
+    let b2 = Board::new(4, 4);
+    b2.print();
 }
