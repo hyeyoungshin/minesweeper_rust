f7c2d607a8a42511128a33ca415c6445d94d8b67
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2025-12-11 21:42:40 -0700

    minor update

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,9 @@ fn main() {
     player_board.print(); // refboard should not be printable
                           // maybe for the debugging purpose, we can print it
                           // playerboard should be inferred from refboard
+    println!();
+
+    let ref_board_with_mines = ref_board.plant_mines(&game::Difficulty::Hard);
+    let new_player_board = ref_board_with_mines.get_playerboard();
+    new_player_board.print();
 }
