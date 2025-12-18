8fc1a4be0efbdedf94281bcc67fb68a5a8d5bcfa
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2025-12-18 15:55:34 -0700

    automatic random play works but ends with either error or over status

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,9 @@
-use crate::board::RefBoard;
-use crate::board::RefTile;
-use crate::board::Coordinate;
-use crate::board::TileStatus;
+pub mod board; 
+
+use board::RefBoard;
+use board::RefTile;
+use board::Coordinate;
+use board::TileStatus;
 
 #[derive(Clone)]
 pub enum Difficulty {
@@ -10,49 +12,43 @@ pub enum Difficulty {
     Hard
 }
 
-struct Game {
-    ref_board: RefBoard,    
-    status: GameStatus,
+pub struct Game {
+    pub ref_board: RefBoard,    
+    pub status: GameStatus,
 }
 
-enum GameStatus {
+#[derive(PartialEq, Debug)]
+pub enum GameStatus {
     Continue,
     Over,
     Error
 }
 
-struct PlayerAction {
-    coordinate: Coordinate,
-    action: Action, 
+pub struct PlayerAction {
+    pub coordinate: Coordinate,
+    pub action: Action, 
 }
 
-#[derive(PartialEq)]
-enum Action{
+#[derive(Debug, PartialEq)]
+pub enum Action{
     Reveal, 
     Flag, 
     Unflag
 }
 
 impl Game {
-    fn new(&self, size: u32, d: Difficulty) -> Game {
-        let new_ref_board = RefBoard::new(size, size);
+    // pub fn is_valid_input_coordinate(&self, input: Vec<&str>) -> bool {
+    //     match input.len() {
+    //         2 => (input.get(0).unwrap() < self.ref_board.x_size) && (input.get(1) < self.ref_board.y_size)
+    //         _ => false
+    //     }
         
-        Game {
-            ref_board: new_ref_board.plant_mines(&d),
-            status: GameStatus::Continue
-        }
-    }
+    // }
 
-    fn make_move(&self, a: PlayerAction) -> Game {
+    pub fn make_move(&self, a: &PlayerAction) -> Game {
         let mut new_board_map = self.ref_board.board_map.clone();
 
         let current_tile = self.ref_board.board_map.get(&a.coordinate).expect("tile must exist");
-
-        let new_tile_status = match a.action {
-            Action::Reveal => TileStatus::Revealed,
-            Action::Flag => TileStatus::Flagged,
-            Action::Unflag => TileStatus::Hidden
-        };
 
         let new_game_status = 
             if is_valid(&current_tile.status, &a.action) {
@@ -64,16 +60,32 @@ impl Game {
             } else {
                 GameStatus::Error
             };
+
+        let new_tile_status = match a.action {
+            Action::Reveal => TileStatus::Revealed,
+            Action::Flag => TileStatus::Flagged,
+            Action::Unflag => TileStatus::Hidden
+        };
+    
         
         new_board_map.insert(a.coordinate, RefTile{has_mine: current_tile.has_mine, status: new_tile_status});
         
         Game {
             ref_board: RefBoard{
-                xsize: self.ref_board.xsize, 
-                ysize: self.ref_board.ysize, 
+                x_size: self.ref_board.x_size, 
+                y_size: self.ref_board.y_size, 
                 board_map: new_board_map},
             status: new_game_status
         }
+    }
+}
+
+pub fn new_game(board_size_x: u32, board_size_y: u32, d: Difficulty) -> Game {
+    let new_ref_board = RefBoard::new(board_size_x, board_size_y);
+    
+    Game {
+        ref_board: new_ref_board.plant_mines(d),
+        status: GameStatus::Continue
     }
 }
 
@@ -84,3 +96,13 @@ fn is_valid(t: &TileStatus, a: &Action) -> bool {
         TileStatus::Revealed => false
     }
 }
+
+pub fn random_action() -> Action {
+    use rand::Rng;
+    match rand::thread_rng().gen_range(0..3) {
+        0 => Action::Reveal,
+        1 => Action::Flag,
+        _ => Action::Unflag,
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,82 @@
 mod game;
-mod board;
+
+use crate::game::*;
+use game::board::*;
+use std::io;
+// use crate::game::Difficulty;
+// use crate::game::GameStatus;
+
 
 fn main() {
-    let ref_board = board::RefBoard::new(4, 4);
-    let player_board = ref_board.get_playerboard();
-    player_board.print(); // refboard should not be printable
-                          // maybe for the debugging purpose, we can print it
-                          // playerboard should be inferred from refboard
-    println!();
+    let mut game = new_game(5,5, Difficulty::Medium);
 
-    let ref_board_with_mines = ref_board.plant_mines(&game::Difficulty::Hard);
-    let new_player_board = ref_board_with_mines.get_playerboard();
-    new_player_board.print();
+
+    // while game.status == GameStatus::Continue {
+    //     println!("Enter a coordinate separated by a comma:");
+
+    //     let mut player_coordinate = String::new();
+    //     io::stdin().read_line(&mut player_coordinate)?;
+
+    //     let chars: Vec<&str> = player_coordinate.trim().split(',').collect();
+        
+    //     // println!("{:?}", chars)
+    //     if is_valid_input(chars) {
+    //         // continue
+    //     } else {
+    //         //
+    //     }
+    // }
+
+    // Ok(())
+
+    /////// automatic game play /////////
+    // plays until game status becomes either over or error
+    while game.status == GameStatus::Continue {
+            let player_coordinate = random_coordinate(game.ref_board.x_size, game.ref_board.y_size);
+        println!("player coordinate: {:?}", player_coordinate);
+        
+        // println!("{}", game.ref_board.num_mines_nearby(&player_coordinate));
+        
+        let player_action = random_action();
+        println!("player action: {:?}", player_action);
+        
+        let action = &PlayerAction {
+            coordinate: player_coordinate,
+            action: player_action
+        };
+        
+        game = game.make_move(action);
+        
+        game.ref_board.print_mines();
+        println!();
+        
+        game.ref_board.get_playerboard().print();
+    }
+
+    println!("{:?}", game.status);
+
+
+    ////////////// The Game Loop ////////////
+    // while game.status == GameStatus::Continue {
+    //     println!("game status:{:?}", game.status);
+
+    //     let player_coordinate = random_coordinate(game.ref_board.x_size, game.ref_board.y_size);
+    //     println!("coordinate: {:?}", player_coordinate);
+        
+    //     let player_action = random_action();
+    //     println!("action: {:?}", player_action);
+        
+    //     let action = &PlayerAction {
+    //         coordinate: player_coordinate,
+    //         action: Action::Flag,
+    //     };
+        
+    //     game = game.make_move(action);
+    //     if game.status == GameStatus::Error {
+    //         println!("Invalid move! Make another move.");
+    //         game.status = GameStatus::Continue
+    //     }
+    // }
+    // println!("game status:{:?}", game.status);
+
 }
